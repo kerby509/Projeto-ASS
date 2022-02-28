bfd79f3aacd4bf42e192f719c329ee9b0d1ba87e
Daniel <pierrelusdaniel@gmail.com>
2022-02-28 18:45:49 -0300

    cadastra cliente

--- cadcliente.php
+++ cadcliente.php
@@ -2,11 +2,54 @@
 
 
 ?>
- <html>
+ <form className="form" action="cadcliente.php" method="post" >
+<table width="80%" border="0" cellspacing="0" cellpadding="0" align="center">
+        
+    <div align="center">
+    <h1>Cadastra Cliente</h1>
+
+    <tr>
+     <td width="24%" height="25"><font face="Arial" size="2">Nome:</font></td>
+     <td height="25" width="76%"><font face="Arial" size="2">
+     <input type="text" name="nome_user" size="35"></font></td>
+    </tr>
+
+    <tr>
+     <td height="25" width="24%"><font face="Arial" size="2">CPF:</font></td>
+     <td height="25" width="76%"><font face="Arial" size="2">
+     <input type="text" name="end_user" size="30"></font></td>
+    </tr>
 
 
+    <tr>
+     <td height="25" width="24%"><font face="Arial" size="2">Telefone:</font></td>
+     <td height="25" width="76%"><font face="Arial" size="2">
+     <input type="text" name="tel_user" size="20"></font></td>
+    </tr>
 
+    <tr>
+     <td height="25" width="24%"><font face="Arial" size="2">Date de entrega:</font></td>
+     <td height="25" width="76%"><font face="Arial" size="2">
+     <input type="text" name="bairro_user" size="30"></font></td>
+    </tr>
 
+    <tr>
+     <td height="25" width="24%"><font face="Arial" size="2">Modelo:</font></td>
+     <td height="25" width="76%"><font face="Arial" size="2">
+     <input type="text" name="email_user" size="35"></font></td>
+    </tr>
 
+    <tr>
+     <td height="25" width="24%"><font face="Arial" size="2">Serviços:</font></td>
+     <td height="25" width="76%"><font face="Arial" size="2">
+     <input type="text" name="tel_user" size="35"></font></td>
+    </tr>
 
- </html>
+    <tr>
+     <td height="20" colspan="2">
+     <div align="center">
+     <input type="submit" name="cadastrar" value="Cadastrar cliente &gt;&gt;"></div></td>
+    </tr>
+   </table>
+  
+ 
