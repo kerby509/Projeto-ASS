5292e0b91f0ab5f2da5643df6ab85f95669bcf5e
kerby <lovincekerby99@gmail.com>
2022-02-28 20:17:29 -0300

    adicionar css

--- login.php
+++ login.php
@@ -1,4 +1,10 @@
 <?php
+ 
+include'css/styles.css';
+
+
+
+
 
 
 
@@ -6,27 +12,44 @@
 ?>
 
 <html>
-    <div>
-        <form className="form" action="login.php" method="post" >
-            <h1>Acessar ao Sistema</h1>
-            <p>Usuário</p>
-            <input type="text" name="usuario">
-            <p>Senha</p>
-            <input type="password" name="senha" > 
-    </div>
+    
 
-    <div className="actions">
-                    <button type="submit">Entrar</button>
-        
+    <div style= "  display:flex ;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+    padding: 0 2rem;
+    background-image: url("assets/fundo.jpeg");">
 
-     </div>
+        <form style=" background:red;
+        width: 100%;
+        max-width: 480px;
+         padding: 0.5rem;s
+         " action="login.php" method="post" >
+            <h1 style= "text-align: center;
+            ">Acessar ao Sistema</h1>
+           <div style= "text-align: center;">
+                <p>Digite seu E-mail</p>
+                <input type="text" name="usuario">
+                <p>Digite sua senha</p>
+                <input type="password" name="senha" > 
+            </div>
+    
 
-     <div className="adm">
-                    
+            <div style="text-align: center;
+                margin-top: 0.5rem;">
+                    <button type="submit">Entrar</button>     
+
+             </div>
+
+             <div style="text-align: center;
+                margin-top: 0.5rem;">                    
              <button type="submit">Cadastra Administrador</button>
-     </div>
+             </div>
        
         
 
-    </form>
+        </form>
+ </div>
 </html>
