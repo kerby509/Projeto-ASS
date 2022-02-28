9974650bf09b26d4cec734f9b987eafcecd38bec
kerby <lovincekerby99@gmail.com>
2022-02-28 13:05:08 -0300

    first commit

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+
+?>
+
+<html>
+    <form action="login.php" method="post" >
+        <p>Usuário</p>
+        <input type="text" name="usuario">
+        <p>Senha</p>
+        <input type="password" name="senha" >
+        <input type="submit" value="Entrar" >
+        <input type="submit" value="Cadastra Administrador" >
+
+    </form>
+</html>
