32b5ccaf92971e45542c7931c640c36552952308
kerby <lovincekerby99@gmail.com>
2022-03-12 11:54:01 -0300

    ajuste css

--- login.php
+++ login.php
@@ -14,24 +14,15 @@ if(@$_POST['usuario']=='megas' &&
 ?>
 
 <html>
-    
+<link rel="stylesheet" href="styles.css">
 
-    <div style= "display:flex ;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    min-height: 100vh;
-    padding: 0 2rem;
-    background-image: url(assets/fundo.jpeg)">
 
-        <form style=" background:red;
-        width: 100%;
-        max-width: 480px;
-         padding: 0.5rem;
-         " action="login.php" method="post" >
-            <h1 style= "text-align: center;
-            ">Acessar ao Sistema</h1>
-           <div style= "text-align: center;">
+    <div class="login" >
+
+        <form 
+          action="login.php" method="post" >
+          <div class="caixa">
+                <h1 >Acessar ao Sistema</h1>
                 <p>Digite seu E-mail</p>
                 <input type="text" name="usuario">
                 <p>Digite sua senha</p>
