45629a294b4de6af9d48968b706b2950470dbfa7
kerby <lovincekerby99@gmail.com>
2022-03-16 20:37:28 -0300

    Aqui é a funcãao para registra cliente

---
+++ RegEntrega.php
@@ -0,0 +1,32 @@
+<?php
+
+
+echo"Aqui é parte de registra entrega"
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<link rel="stylesheet" href="styles.css">
+    
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width= device-width">
+    <title>Registra Cliente</title>
+    
+
+    </head>
+<body>
+    <header>
+        <h1>Registra Cliente<h1>
+    </header>
+
+    <div>
+        <form>
+            
+        </form>
+
+    </div>
+
+</body>
