c4ea052cc57dc765aec7a82d2486dd8402eb6797
kerby <lovincekerby99@gmail.com>
2022-03-16 20:53:34 -0300

    commit

--- RegEntrega.php
+++ RegEntrega.php
@@ -22,11 +22,30 @@ echo"Aqui é parte de registra entrega"
         <h1>Registra Cliente<h1>
     </header>
 
-    <div>
-        <form>
-            
+    
+        <form className="form" action="RegEntrega.php" method="post">
+            <fieldset>
+                <div>
+                    <label>CPF</label>
+                    <input type="text" name="cpf">
+                </div>
+                <div>
+                    <label>Nome</label>
+                    <input type="text" name="nome" id="nome">
+                </div>
+                <div>
+                    <label>Serviços</label>
+                    <input type="text" name="servicos">
+                </div>
+                <div>
+                <label>F.Pagamento</label>
+                <input type="text" name="f.pagamento">
+
+                </div>
+            </fieldset>
+
         </form>
 
-    </div>
+    
 
 </body>
