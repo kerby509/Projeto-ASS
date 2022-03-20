b52a82b332ee105c39bbe135ad9557e518dc1539
kerby <lovincekerby99@gmail.com>
2022-03-20 13:27:07 -0300

    commit

--- cadcliente.php
+++ cadcliente.php
@@ -1,16 +1,15 @@
 <?php
-include 'db.php';
+//include 'db.php';
 session_start();
 
-if(@$_POST['nome']&&
-  @_POST['email']&&
-  @$_POST['cpf']&&
+if(@$_POST['cpf']&&
+  @_POST['nome']&&
+  @$_POST['email']&&
   @$_POST['telefone']&&
   @$_POST['data de entrega']&&
   @$_POST['modelo']&&
-  @$_POST['serviços']&&
-  @_POST['senha']){
-    $sql=$db->prepare("INSERT INTO usuarios(nome,email,cpf,telefone,datadeentrega,modelo,serviços,senha) VALUES (:nome,:email,:cpf,:telefone,:data de entrega,:modelo,serviços,senha)");
+  @$_POST['servicos']){
+    $sql=$db->prepare("INSERT INTO usuarios(cpf,nome,email,telefone,datadeentrega,modelo,servicos) VALUES ,:cpf,:nome,:email,:telefone,:data de entrega,:modelo,servicos)");
     $sql->bindParam('nome', $_POST['nome'], PDO::PARAM_STR);
     $sql->bindParam('email', $_POST['email'], PDO::PARAM_STR);
     $sql->bindParam('cpf', $_POST['cpf'], PDO::PARAM_STR);
@@ -18,7 +17,6 @@ if(@$_POST['nome']&&
     $sql->bindParam('data de entrega', $_POST['data de entrega']);
     $sql->bindParam('modelo', $_POST['modelo'], PDO::PARAM_STR);
     $sql->bindParam('serviços', $_POST['serviços'], PDO::PARAM_STR);
-    $sql->bindParam('senha', $_POST['senha'], PDO::PARAM_STR);
     $sql->execute();
 
       $_SESSION['autenticados']=1;
@@ -31,70 +29,109 @@ if(@$_POST['nome']&&
   //var_dump($_POST);
   //exit();
 ?>
- <form className="form" action="cadcliente.php" method="post" >
-<table width="80%" border="0" cellspacing="0" cellpadding="0" align="center">
-        
-    <div align="center">
-    <h1>Cadastra Cliente</h1>
-
-    <tr>
-     <td width="24%" height="25"><font face="Arial" size="2">Nome:</font></td>
-     <td height="25" width="76%"><font face="Arial" size="2">
-     <input type="text" name="nome" size="35"></font></td>
-    </tr>
-
-    <tr>
-     <td width="24%" height="25"><font face="Arial" size="2">Email:</font></td>
-     <td height="25" width="76%"><font face="Arial" size="2">
-     <input type="text" name="email" size="35"></font></td>
-    </tr>
-
-    <tr>
-     <td height="25" width="24%"><font face="Arial" size="2">CPF:</font></td>
-     <td height="25" width="76%"><font face="Arial" size="2">
-     <input type="text" name="cpf" size="30"></font></td>
-    </tr>
 
 
-    <tr>
-     <td height="25" width="24%"><font face="Arial" size="2">Telefone:</font></td>
-     <td height="25" width="76%"><font face="Arial" size="2">
-     <input type="text" name="telefone" size="20"></font></td>
-    </tr>
+<!DOCTYPE html>
+<html lang="pt-br">
+<link rel="stylesheet" href="caixa.css">
 
-    <tr>
-     <td height="25" width="24%"><font face="Arial" size="2">Date de entrega:</font></td>
-     <td height="25" width="76%"><font face="Arial" size="2">
-     <input type="date" name="data de entrega" 
-        placeholder="dd-mm-yyyy" value=""
-        min="1997-01-01" max="2030-12-31">
-     
-    </tr>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cadastra Cliente</title>
+    
+</head>
+<body>
 
-    <tr>
-     <td height="25" width="24%"><font face="Arial" size="2">Modelo:</font></td>
-     <td height="25" width="76%"><font face="Arial" size="2">
-     <input type="text" name="modelo" size="35"></font></td>
-    </tr>
+<br>
+<h1>Cadastra Cliente</h1>
 
-    <tr>
-     <td height="25" width="24%"><font face="Arial" size="2">Serviços:</font></td>
-     <td height="25" width="76%"><font face="Arial" size="2">
-     <input type="text" name="serviços" size="35"></font></td>
-    </tr>
 
-    <tr>
-     <td width="24%" height="25"><font face="Arial" size="2">Cria a Senha:</font></td>
-     <td height="25" width="76%"><font face="Arial" size="2">
-     <input type="text" name="senha" size="35"></font></td>
-    </tr>
+<div class="container">
+  <a href="index.php"><button>Voltar</button></a>
+ 
+  <form action="cadcliente.php" method= "POST">
+  <div class="row">
+    <div class="col-25">
+      <label for="cpf">CPF</label>
+    </div>
+    <div class="col-75">
+      <input type="text" id="cpf" name="cpf" placeholder=" Digite cpf "required>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-25">
+      <label for="nome">Nome</label>
+    </div>
+    <div class="col-75">
+      <input type="text" id="nome" name="nome" placeholder="Digite nome"required>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-25">
+      <label for="modelo">E-mail</label>
+    </div>
+    <div class="col-75">
+      <input type="text" id="email" name="email" placeholder="Digite e-mail">
+    </div>
+  </div>
 
-    <tr>
-     <td height="20" colspan="2">
-     <div align="center">
-     <input type="submit" name="cadastrar" value="Cadastrar cliente &gt;&gt;"></div></td>
-    </tr>
-   </table> 
-   </form>
+  <div class="row">
+    <div class="col-25">
+      <label for="telefone">Telefone</label>
+    </div>
+    <div class="col-75">
+      <input type="text" id="telefone" name="telefone" placeholder="(xx)xxxxxxxxx"required>
+    </div>
+    
+  </div>
+
+  <div class="row">
+    <div class="col-25">
+      <label for="telefone">Data Entrega</label>
+    </div>
+    <div class="col-75">
+      <input type="date" id="data de entrega" name="data de entrega" placeholder="dd-mm-yyyy"required>
+    </div>
+    
+  </div>
+
+  <div class="row">
+    <div class="col-25">
+      <label for="modelo">Modelo</label>
+    </div>
+    <div class="col-75">
+      <input type="text" id="modelo" name="modelo" placeholder="Digite o modelo"required>
+    </div>
+  </div>
+
+
+  <div class="row">
+    <div class="col-25">
+      <label for="servico">Serviços</label>
+    </div>
+    <div class="col-75">
+      <input type="text" id="servicos" name="servicos" placeholder="Digite o serviço realizado" required>
+    </div>
+  </div>
+
   
+   
+
+ 
+  <br>
+  <br>
+  <div class="row">
+    <input type="submit" value="Cadastra Cliente" name="update"/>
+  </div>
+  </form>
+</div>
+
+</body>
+</html>
+
+
+
+
  
