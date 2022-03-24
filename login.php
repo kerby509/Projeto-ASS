4deace7657a6cdfcaaeab3699c0495d3d0704be7
kerby <lovincekerby99@gmail.com>
2022-03-24 15:39:57 -0300

    commit

--- login.php
+++ login.php
@@ -1,53 +1,19 @@
 <?php
  
 include'css/styles.css';
-include'db.php';
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-// sa c pou lew fin fe cadastra a pou cliente resgistre nan sistem nan
-// $sql=$db->prepare("SELECT * FROM usuarios WHERE email=:email and senha=:senha");
-// $sql->bindParam('email', $_POST['email'], PDO::PARAM_STR);
-// $sql->bindParam('senha', $_POST['senha'], PDO::PARAM_STR);
-// $sql->execute();
-// $data = $sql->fetch();
-
-
-//pukisa c nn login nn ou fè saaa
-<<<<<<< HEAD
-$comando=$db->prepare("SELECT * FROM funcionario WHERE email=:email and senha=:senha");
-$comando->bindParam('email', $_POST['email'], PDO::PARAM_STR);
-$comando->bindParam('senha', $_POST['senha'], PDO::PARAM_STR);
-$$comando->execute();
-$data = $comando->fetch();
-=======
-<<<<<<< HEAD
-// $sql=$db->prepare("SELECT * FROM usuarios WHERE email=:email and senha=:senha");
-// $sql->bindParam('email', $_POST['email'], PDO::PARAM_STR);
-// $sql->bindParam('senha', $_POST['senha'], PDO::PARAM_STR);
-// $sql->execute();
-// $data = $sql->fetch();
-=======
-// $comando=$db->prepare("SELECT * FROM usuarios WHERE email=:email and senha=:senha");
-// $comando->bindParam('email', $_POST['email'], PDO::PARAM_STR);
-// $comando->bindParam('senha', $_POST['senha'], PDO::PARAM_STR);
-// $$comando->execute();
-// $data = $comando->fetch();
->>>>>>> 527898ccc7cf624955c63bd1aec4c5b250498621
-
->>>>>>> 5b252521811a4b54a9fe79e616c93b000aaa35b8
-
 session_start();
-if($data){
-    // if(@$_POST['usuario']==''&&
-    // @$_POST['senha']==''){
+//if($data){
+    if(@$_POST['usuario']=='megas'&&
+    @$_POST['senha']=='1234'){
         $_SESSION['autenticados']=1;
         header('Location: index.php');
         exit();
     }
+
+
 //include 'barra_de_navigação.php';
+
+
 ?>
 
 <html>
