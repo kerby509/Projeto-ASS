4916ca7df8ad3df18eb02ea4007c9ce1f9edf101
Daniel <pierrelusdaniel@gmail.com>
2022-03-24 15:36:56 -0300

    commit

--- cadcliente.php
+++ cadcliente.php
@@ -54,63 +54,6 @@ try{
 
 ?>
 
- <!-- <form className="form" action="cadcliente.php" method="post" >
-<table width="80%" border="0" cellspacing="0" cellpadding="0" align="center">
-        
-    <div align="center">
-    <h1>Cadastra Cliente</h1>
-
-    <tr>
-     <td width="24%" height="25"><font face="Arial" size="2">Nome:</font></td>
-     <td height="25" width="76%"><font face="Arial" size="2">
-     <input type="text" name="nome" size="20"></font></td>
-    </tr>
-
-    
-    <tr>
-     <td height="25" width="24%"><font face="Arial" size="2">CPF:</font></td>
-     <td height="25" width="76%"><font face="Arial" size="2">
-     <input type="text" name="cpf" size="20"></font></td>
-    </tr>
-
-
-    <tr>
-     <td height="25" width="24%"><font face="Arial" size="2">Telefone:</font></td>
-     <td height="25" width="76%"><font face="Arial" size="2">
-     <input type="text" name="telefone" size="20"></font></td>
-    </tr>
-
-    <tr>
-     <td height="25" width="24%"><font face="Arial" size="2">Date de entrega:</font></td>
-     <td height="25" width="76%"><font face="Arial" size="2">
-     <input type="date" name="data de entrega"
-        placeholder="dd-mm-yyyy" value=""
-        min="1997-01-01" max="2030-12-31">
-      
-     
-    </tr>
-
-    <tr>
-     <td height="25" width="24%"><font face="Arial" size="2">Modelo:</font></td>
-     <td height="25" width="76%"><font face="Arial" size="2">
-     <input type="text" name="modelo" size="20"></font></td>
-    </tr>
-
-    <tr>
-     <td height="25" width="24%"><font face="Arial" size="2">Serviços:</font></td>
-     <td height="25" width="76%"><font face="Arial" size="2">
-     <input type="text" name="serviços" size="20"></font></td>
-    </tr>
-
-
-    <tr>
-     <td height="30" colspan="2">
-     <text-align="center">
-     <input type="submit"name="cadastrar" value="Cadastrar cliente &gt;&gt;"/>
-     
-    </tr>
-   </table> 
-   </form> -->
    
 <!DOCTYPE html>
 <html lang="pt-br">
