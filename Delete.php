a5b7f4ac9f5d3cd52fe475eab3021251d37f7367
Daniel <pierrelusdaniel@gmail.com>
2022-03-31 15:34:24 -0300

    commit

--- Delete.php
+++ Delete.php
@@ -1,9 +1,46 @@
+
 <?php
-
-
-
-
-?>
+     
+     include 'db.php';
+     
+     // ini_set('display_errors', 1);
+     // ini_set('display_startup_errors', 1);
+     // error_reporting(E_ALL);
+     
+     try {
+     
+         $cpf = '' ;
+         $nome = '';
+         $email = '';
+         $endereco='';
+         $telefone='';
+         $senha= '';
+         
+         if(@$_POST['cpf']){
+             $cpf= $_POST['cpf'];
+           
+         $comando = $db->prepare('SELECT cpf FROM funcionario WHERE cpf = :cpf');
+         $comando->bindParam(':cpf', $cpf);
+         $comando->execute();
+         $data=$comando->fetch();
+         if(!$data){
+             echo ' cpf não existe';
+         }
+     
+         $comando = $db->prepare('DELETE FROM funcionario WHERE cpf=:cpf');
+         $comando->bindParam(':cpf', $cpf);
+     
+         $comando->execute();
+     
+     
+         }
+     }catch (PDOException $e) {
+         echo 'Erro ao executar comando no banco de dados: ' . $e->getMessage();
+         exit();
+     }
+     
+     ?>
+     
 
 
 <!DOCTYPE html>
@@ -37,14 +74,14 @@
       <input type="text" id="cpf" name="cpf" placeholder=" Digite cpf cliente "required>
     </div>
   </div>
-  <div class="row">
+  <!-- <div class="row">
     <div class="col-25">
       <label for="nome">Nome</label>
     </div>
     <div class="col-75">
       <input type="text" id="nome" name="nome" placeholder="Digite nome do cliente"required>
     </div>
-  </div>
+  </div> -->
   <div class="row">
     <input type="submit" value="Delete Cliente" name="update"/>
   </div>
